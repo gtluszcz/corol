327cc4c9ae57323d287a8b9896d693efa285bdd1
gtluszcz <gtluszcz@gmail.com>
2020-12-05 14:24:44 +0100

    rename function

--- test/hsl.test.js
+++ test/hsl.test.js
@@ -1,7 +1,7 @@
 import HSL from '../src/colorspaces/hsl'
 import { ColorError } from '../src/errors'
 
-function dummyhsl() {
+function dummyHSL() {
     return new HSL({h: 100, s: 0.5, l: 0.5})
 }
 
@@ -48,64 +48,64 @@ describe('HSL constructor', () => {
 describe('HSL field setters', () => {
 
     test('doesn\'t throw when setting proper h', () => {
-        const hsl = dummyhsl()
+        const hsl = dummyHSL()
         expect(() => {hsl.h = 100}).not.toThrow()
     });
 
     test('doesn\'t throw when setting h to 0', () => {
-        const hsl = dummyhsl()
+        const hsl = dummyHSL()
         expect(() => {hsl.h = 0}).not.toThrow()
     });
 
     test('doesn\'t throw when setting h to 360', () => {
-        const hsl = dummyhsl()
+        const hsl = dummyHSL()
         expect(() => {hsl.h = 360}).not.toThrow()
     });
 
     test('throws when setting too low h', () => {
-        const hsl = dummyhsl()
+        const hsl = dummyHSL()
         expect(() => {hsl.h = -1}).toThrow(ColorError)
     });
     
     test('throws when setting too high h', () => {
-        const hsl = dummyhsl()
+        const hsl = dummyHSL()
         expect(() => {hsl.h = 361}).toThrow(ColorError)
     });
     
     test('throws when setting h to NaN', () => {
-        const hsl = dummyhsl()
+        const hsl = dummyHSL()
         expect(() => {hsl.h = NaN}).toThrow(ColorError)
     });
 
 
     ['s','l'].forEach(field => {
         test(`doesn't throw when setting proper ${field}`, () => {
-            const hsl = dummyhsl()
+            const hsl = dummyHSL()
             expect(() => {hsl[field] = 0.5}).not.toThrow()
         });
     
         test(`doesn't throw when setting ${field} to 0`, () => {
-            const hsl = dummyhsl()
+            const hsl = dummyHSL()
             expect(() => {hsl[field] = 0}).not.toThrow()
         });
     
         test(`doesn't throw when setting ${field} to 1`, () => {
-            const hsl = dummyhsl()
+            const hsl = dummyHSL()
             expect(() => {hsl[field] = 1}).not.toThrow()
         });
     
         test(`throws when setting too low ${field}`, () => {
-            const hsl = dummyhsl()
+            const hsl = dummyHSL()
             expect(() => {hsl[field] = -0.1}).toThrow(ColorError)
         });
         
         test(`throws when setting too high ${field}`, () => {
-            const hsl = dummyhsl()
+            const hsl = dummyHSL()
             expect(() => {hsl[field] = 1.1}).toThrow(ColorError)
         });
         
         test(`throws when setting ${field} to NaN`, () => {
-            const hsl = dummyhsl()
+            const hsl = dummyHSL()
             expect(() => {hsl[field] = NaN}).toThrow(ColorError)
         });
     })
